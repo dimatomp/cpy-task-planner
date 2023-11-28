87159650e1e38e4fbb0c7350f7f7a0d512314472
Dmitry Tomp <dmitry.tomp@rakuten.com>
2023-11-28 12:49:35 +0900

    Add package name

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 import os
 
-setup(ext_modules = cythonize([
-    Extension('taskplanner', ["taskplanner.pyx"], language="c++", extra_compile_args=["-I" + os.getenv('LEMON_INCLUDE_PATH'), "-std=c++11"])
+setup(
+    name="cpy-task-planner",
+    ext_modules = cythonize([
+        Extension('taskplanner', ["taskplanner.pyx"], language="c++", extra_compile_args=["-I" + os.getenv('LEMON_INCLUDE_PATH'), "-std=c++11"])
 ]))
